7f091049bd15a0692c7f8825677d2b383c82b7f6
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2023-02-09 00:02:10 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,13 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://scintillating-pie-9efb4b.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 
 app.get("/", async (req, res) => {
   res.json({ message: "Please visit /countries to view all the countries" });
@@ -19,9 +26,3 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173/');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
